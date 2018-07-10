7e0dd1a301f9841b5349074699812f2b3a8e958e
Ana Bebic <anabebic.x@gmail.com>
2018-07-10 19:15:06 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_reader :name 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def match(arr)
+    array.each{|x| x ==x}
+    
+  end
+  
+end
